ea7c36e9ef5071e53fe9a2bd5db1a6914aef5ae7
Yiown Sugpo <yiown@hotmail.com>
2011-05-14 21:38:21 -0300

    Initial database: playlist, song, vote.

--- radiohub/models.py
+++ radiohub/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    path = models.FilePathField(max_length=200)
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200)
+    criteria = models.CharField(max_length=200)
+    consume = models.BooleanField()
+    private = models.BooleanField()
+
+class Vote(models.Model):
+    playlist = models.ForeignKey(Playlist)
+    song = models.ForeignKey(Song)
+    vote = models.SmallIntegerField()
+
